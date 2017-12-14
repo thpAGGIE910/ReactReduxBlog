c68abed4c37601e2710548b0d6e8b79fd8ea3aa3
Travis Payne <thpAGGIE910@gmail.com>
2017-12-14 00:10:55 -0600

    Signal the create post action creator on post form submission

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -21,7 +24,7 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values);
   }
 
   render() {
@@ -75,4 +78,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);

cce3327e42795cadc59bb6d8943ce37499307f40
Travis Payne <thpAGGIE910@gmail.com>
2017-12-13 23:10:47 -0600

    Add a `PostsNew` component that allows the user to create a new post

---
+++ src/components/posts_new.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor="title">{field.label}</label>
+        <input 
+          type="text"
+          className="form-control"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        <Field 
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        ></Field>
+      </form>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);

091f2701ae845c3cf3dd04d5439a918c00e81ae4
Travis Payne <thpAGGIE910@gmail.com>
2017-12-13 23:57:46 -0600

    Wireup form validation with redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label htmlFor="title">{field.label}</label>
         <input 
           type="text"
           className="form-control"
           {...field.input}
         />
+        <div className="text-help">{touched ? error : ''}</div> 
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field 
           label="Title"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
 
@@ -34,11 +45,34 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         ></Field>
+        <button type="submit" className="btn btn-primary">Save</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters!";
+  }
+
+  if(!values.categories) {
+    errors.categories = "Enter at least one category!";
+  }
+
+  if(!values.content) {
+    errors.content = "Please enter some blog content!";
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);

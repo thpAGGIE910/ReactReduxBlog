6b0f95920e16485a001a8211a719966fe116b4a9
Travis Payne <thpAGGIE910@gmail.com>
2017-12-14 11:46:34 -0600

    Create a new action creator to update the state when an individual post is created and register the action in the posts reducer.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,17 @@
 import _ from "lodash";
 
-import { FETCH_POSTS } from "../actions";
+import { FETCH_POSTS, FETCH_POST } from "../actions";
 
 export default function(state = {}, action) {
   switch(action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, "id");
+
+    case FETCH_POST:
+      const post = action.payload.data;
+
+      return { ...state, [action.payload.data.id]: action.payload.data };
+    
     default:
       return state;
   }

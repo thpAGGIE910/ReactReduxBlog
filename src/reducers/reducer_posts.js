c5321fa5e60acd2d6a6a0ae01f9214eec7ba3b41
Travis Payne <thpAGGIE910@gmail.com>
2017-12-16 11:59:53 -0600

    Implement the functionality to allow a user to delete a specific post.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 
-import { FETCH_POSTS, FETCH_POST } from "../actions";
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from "../actions";
 
 export default function(state = {}, action) {
   switch(action.type) {
@@ -9,8 +9,10 @@ export default function(state = {}, action) {
 
     case FETCH_POST:
       const post = action.payload.data;
-
       return { ...state, [action.payload.data.id]: action.payload.data };
+
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     
     default:
       return state;
